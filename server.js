bc17e7ecb201d37868ac327578c0798f08225e2d
dwelings <143724350+dwelings@users.noreply.github.com>
2024-09-06 09:54:24 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+// server.js
+const express = require('express');
+const request = require('request');
+const app = express();
+const port = 3000;
+
+app.get('/proxy', (req, res) => {
+    const url = req.query.url;
+
+    if (!url) {
+        return res.status(400).send('URL query parameter is required.');
+    }
+
+    request({ url: url, method: 'GET' })
+        .on('error', (err) => {
+            res.status(500).send('Error fetching the URL.');
+        })
+        .pipe(res);
+});
+
+app.listen(port, () => {
+    console.log(`Proxy server listening at http://localhost:${port}`);
+});
 
